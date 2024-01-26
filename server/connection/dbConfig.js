e352a0efa05a54df4e8ea67a65ede4617d1d6bbb
xyz <xyz@gmail.com>
2024-01-26 20:22:49 +0530

    Update CinePlex 26-jan-2024 08:22:PM

--- server/connection/dbConfig.js
+++ server/connection/dbConfig.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
       try {
+            // await mongoose.connect("mongodb://127.0.0.1:27017/CinePlex");
             await mongoose.connect("mongodb+srv://vikasmohansingh19:cineplextheonlineticketbookingplatform@cineplexclustor0.pcnkyca.mongodb.net/CinePlex");
             console.log("Database Connected successfully..!!");
       } catch (err) {
